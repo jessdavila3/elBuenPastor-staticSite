b5a3dce3d50f6f04611876d78d168d2ff052a82b
Jesse Davila <jessedavila3@gmail.com>
2017-04-27 10:52:28 -0500

    added jquery to add toggle function to hours info.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+"use strict"
+
+$(document).ready(function() {
+
+    $("#sundayHours").click(function() {
+        $("#sundayInfo").slideToggle( "slow")
+    });
+
+    $("#wednesdayHours").click(function() {
+        $("#wednesdayInfo").slideToggle( "slow")
+    });
+
+});
